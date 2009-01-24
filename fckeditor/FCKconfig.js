4520e64b4e4e0d5730bc8929ee1832786731fbd4
Nat Welch <nat@natwelch.com>
2009-01-23 16:14:00 -0800

    Whee javascript.

---
+++ fckeditor/FCKconfig.js
@@ -0,0 +1,12 @@
+FCKConfig.ToolbarSets["NatToolbar"] = [
+['Cut','Copy','Paste','PasteText','PasteWord'],
+	['Undo','Redo','-','Bold','Italic','Underline','StrikeThrough'],
+	'/',
+	['OrderedList','UnorderedList','-','Outdent','Indent'],
+	['Link','Unlink','Anchor'],
+	'/',
+	['Style'],
+	['Table','Image','Flash','Rule','SpecialChar'],
+	['About']
+	] ;
+
