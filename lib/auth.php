67139b877f36b8085dfeb413b51b5f683aebeca5
Nat Welch <nat@natwelch.com>
2009-02-19 13:57:15 -0800

    Just writing down some ideas and adding a git ignore

---
+++ lib/auth.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * What cookie generation technique should I use?
+ * Currently storring md5(pw) and user email. Maybe hash IP address and Md5(pw)?
+ * to think about...
+ *
+ * Also need to start restructuring and moving stuff in here.
+ */
+?>
